5914bc6d3504221433eb6a9c799d9da6a1093618
sdlima4025 <sergiolimafg3>
2025-12-21 22:21:11 -0300

    add font Plus jakarta Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/ui/footer.js";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus_jakarta_sans",
   subsets: ["latin"],
 });
 
@@ -24,12 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-br">
+      <body className={`${plusJakartaSans.variable} antialiased`}>
         {/* Render de toda a pagina */}
-        {children} 
+        {children}
         <Footer />
       </body>
     </html>
